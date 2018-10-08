97fb397162b242c9f3819a84e1e2b3217b6a3889
Dan Leehr <dan.leehr@duke.edu>
2018-10-08 15:17:54 -0400

    Initial attempt at PG version

--- 4.4/RT_SiteConfig.pm
+++ 4.4/RT_SiteConfig.pm
@@ -2,5 +2,10 @@ use utf8;
 
 Set($WebPort, 8080);
 Set($MailCommand, "testfile");
+Set($DatabaseType, "Pg");
+Set($DatabaseHost, "db");
+Set($DatabaseUser, "rt");
+Set($DatabasePassword, "rt_pass");
+Set($DatabaseName, "rt");
 
 1;
